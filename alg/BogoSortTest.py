7abe8b8226836ed33b9513457cdd649400a78710
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-03-31 14:15:22 +0300

    add BogoSort

--- alg/BogoSortTest.py
+++ alg/BogoSortTest.py
@@ -1,7 +1,19 @@
-# импорт класса MonteCarlo
-# from MonteCarlo import MonteCarlo
+# импорт класса BogoSort
+from BogoSort import BogoSort
+# импорт модуля random
+# shuffle() - перемешивает список случайным образом
+from random import shuffle
 
 # тест
 # если не модуль, то выполнить
 if __name__ == '__main__':
-    pass
+    # создаем список
+    numbers = list(range(10))
+    # перемешиваем начальный список
+    shuffle(numbers)
+    # выводим начальный список
+    print(numbers)
+    # сортируем список болотной сортировкой
+    sorted_numbers = BogoSort().bogosort(numbers)
+    # выводим отсортированный список
+    print(sorted_numbers)
